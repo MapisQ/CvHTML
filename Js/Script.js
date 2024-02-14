1e1f373a36acfcd59039cb111dd4a4e8a54056c5
Maria <mapis2321@gmail.com>
2024-02-14 11:54:16 -0500

    Se agregó otra función en el proyecto

--- Js/Script.js
+++ Js/Script.js
@@ -1,9 +1,12 @@
 console.log("Hoja de Vida Interactiva Cargada");
 
 const Idiomas = ["Inglés A2", "Francés B1"];
+const certificaciones = ["Certificado Fundamentos de Programación con Python",
+ "Certificado Internacional de Lengua Francesa DELF A2",
+  "Certificado Internacional de Lengua Francesa DELF A1"];
 
 function mostrarIdiomas() {
-    let ul = document.getElementById('listaIdiomas'); // Suponiendo que tienes un <ul id="listaCertificaciones"> en tu HTML
+    let ul = document.getElementById('listaIdiomas');
     
     for (let i = 0; i < Idiomas.length; i++) {
         let li = document.createElement('li');
@@ -11,10 +14,10 @@ function mostrarIdiomas() {
         ul.appendChild(li);
     }
 
-    window.alert = (" función ejecutada correctamente");
+    alert("Función ejecutada correctamente");
 }
 
-window.onload = mostrarIdiomas();
+window.onload = mostrarIdiomas(); // Sin paréntesis
 
 function añadirHabilidad() {
     let newSkill = prompt("Introduce una nueva habilidad:");
@@ -28,3 +31,26 @@ function añadirHabilidad() {
 document.getElementById('addSkill').addEventListener('click', añadirHabilidad);
 
 
+
+
+function mostrarCertificaciones() {
+     // Solicitar al usuario la cantidad de proyectos a ver
+     const cantidadcertificaciones = prompt("¿Cuántos proyectos deseas ver?");
+            
+     // Verificar si el usuario ingresó un número válido
+     if (cantidadcertificaciones !== null && !isNaN(cantidadcertificaciones) && cantidadcertificaciones !== "") {
+         // Convertir la cantidad ingresada a un número entero
+         const cantidad = parseInt(cantidadcertificaciones);
+         
+         // Verificar si la cantidad ingresada es válida
+         if (cantidad > 0 && cantidad <= certificaciones.length) {
+             // Mostrar los proyectos solicitados
+             for (let i = 0; i < cantidad; i++) {
+                 alert(certificaciones[i]);
+             }
+         } else {
+             alert("La cantidad ingresada no es válida.");
+         }
+    }
+}
+document.getElementById("certificaciones").addEventListener("click", mostrarCertificaciones);
